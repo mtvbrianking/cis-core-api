d4f635e49821c3701cb5a5837c58ecb0918eefc3
mtvbrianking <mtvbrianking@gmail.com>
2019-08-31 12:28:02 +0300

    Passport: Publish components.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,5 +26,13 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        // Passport::loadKeysFrom('/secret-keys/oauth');
+
+        Passport::tokensExpireIn(now()->addDays(15));
+
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
