0287f6c1587b0cf4b3eb6c032d412b302c0c2919
mtvbrianking <mtvbrianking@gmail.com>
2019-09-03 12:22:36 +0300

    Minify assets.

--- resources/views/routes.blade.php
+++ resources/views/routes.blade.php
@@ -4,38 +4,7 @@
 {{-- Excel; html5 + jszip --}}
 <script src="{{ asset('vendor/dataTables.net-buttons/js/buttons.html5.min.js') }}"></script>
 <script src="{{ asset('vendor/jszip/dist/jszip.min.js') }}"></script>
-
-<script type="text/javascript">
-    $(document).ready(function () {
-        var routes_dt = $('table[id=routes]').DataTable({
-            pageLength: 10,
-            language: {
-                emptyTable: "No routes available",
-                info: "Showing _START_ to _END_ of _TOTAL_ routes",
-                infoEmpty: "Showing 0 to 0 of 0 routes",
-                infoFiltered: "(filtered from _MAX_ total routes)",
-                lengthMenu: "Show _MENU_ routes",
-                search: "Search routes:",
-                zeroRecords: "No routes match search criteria"
-            },
-            order: [
-                [1, 'asc'],
-            ],
-            buttons: [
-                {
-                    extend: 'excel',
-                    className: 'btn btn-sm',
-                    text: '<i class="fa fa-file-excel-o"></i> Excel',
-                    exportOptions: {
-                        columns: [0,1,2,3,4]
-                    }
-                }
-            ]
-        });
-
-        routes_dt.buttons().container().appendTo('.export-btns');
-    });
-</script>
+<script src="{{ asset('js/routes.min.js') }}"></script>
 @endpush
 
 @section('content')
