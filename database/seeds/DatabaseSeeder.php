6985463f20d7a1c7467a604b25ef52837e11b3a7
Raymond Nambaale <nambaaler@gmail.com>
2019-10-06 23:42:14 +0300

    rearrange seeding order

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(OauthClientsTableSeeder::class);
         $this->call(FacilitiesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
         $this->call(ModulesTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
