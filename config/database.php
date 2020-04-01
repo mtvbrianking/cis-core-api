62d934754da7aa1c46d2f8acda472d564e4368f4
mtvbrianking <mtvbrianking@gmail.com>
2020-04-01 16:48:35 +0300

    Fix postgres db config

--- config/database.php
+++ config/database.php
@@ -101,16 +101,11 @@ return [
             'database' => 'travis_pgsql_test_db',
             'username' => 'postgres',
             'password' => '',
-            'unix_socket' => '',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'schema' => 'public',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
